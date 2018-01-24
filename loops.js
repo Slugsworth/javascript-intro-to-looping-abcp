156dd69c2cbf4bd4ba10bb045c6cc45760e3b2df
Rick Battipede <rbattipede@gmail.com>
2018-01-24 02:00:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,12 +2,12 @@ var happy = []
 
 function forLoop(array){
   for(var i =0; i<25; i++){
-    if (var i = 1) 
-    array.push("I am 1 strange loop.")
-    else(
+    if (var i = 1) {
+    array.push("I am 1 strange loop.")}
+    else{
       array.push(`I am ${i} strange loop.`)
   }
-}
+}}
 
 function whileLoop(number){
   while(number>0){
