eb1b24e433da81c3c73376793665fb05e79e01c3
Rick Battipede <rbattipede@gmail.com>
2018-01-24 01:35:56 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,25 @@
+var happy = []
+
+function forLoop(array){
+  for(var i =0; i<25; i++){
+    array.push("I am the" + i + "strange loops.")
+  }
+}
+
+function whileLoop(number){
+  while(number>0){
+    console.log(number--)}
+ console.log("done")
+    }
+  
+  var sandwich = ["bread","jam","honey"]
+  
+  function doWhileLoop(array){
+    length = array.length
+    return (length--)
+    do {return array}
+    while(length>0);
+  }
+  
+  
+  
