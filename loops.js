39434becffc796d24b7b7f753c1be151abdd2c23
Rick Battipede <rbattipede@gmail.com>
2018-01-24 01:55:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,8 @@ var happy = []
 
 function forLoop(array){
   for(var i =0; i<25; i++){
-    array.push("I am the" + i + "strange loops.")
+    if {
+      var i = 1} array.push("I am the" + i + "strange loops.")
   }
 }
 
